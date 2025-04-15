2c14ae3bbdb674d4adf4be8531e63e014661a769
Yarakaraju Aditya <144033691+aditya0589@users.noreply.github.com>
2025-04-15 19:25:05 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,26 @@
+/*
+The following script is used to create a database and create three schemas within the database. 
+The script creates a database called as DataWarehouse if it does not already exists
+then it creates three schemas bronze, silver and gold (in accordance with the medallic data engineering model) within the database
+
+Note: if the database DataWarehouse already exists, then it is not again created, so make sure such a database does not exist or is empty. 
+
+creating using mysql. 
+*/
+
+
+
+
+SHOW DATABASES;
+
+CREATE DATABASE IF NOT EXISTS DataWarehouse;
+
+USE DataWarehouse;
+
+CREATE SCHEMA bronze;
+
+CREATE SCHEMA silver;
+
+CREATE SCHEMA gold;
+
 
